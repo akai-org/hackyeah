9fe94af992826686f2017d637ac4ac557783bbbc
Mateusz Stempniewicz <mateusz.stempniewicz@gmail.com>
2017-10-28 23:36:47 +0200

    Articles structure

--- src/components/AddArticleForm.js
+++ src/components/AddArticleForm.js
@@ -15,6 +15,7 @@ class AddArticleForm extends Component {
     this.changeArticleType = this.changeArticleType.bind(this);
     this.returnMoneyTemplate = this.returnMoneyTemplate.bind(this);
     this.returnFoodTemplate = this.returnFoodTemplate.bind(this);
+    this.addArticle = this.addArticle.bind(this);
   }
 
   returnTypeArticle() {
@@ -27,7 +28,7 @@ class AddArticleForm extends Component {
 
   returnMoneyTemplate() {
     return (
-      <div>
+      <div className="wrap-input-container">
         <label></label>
         <input type="number" id="moneyValue" name="moneyValue" placeholder="1 Euro" />
       </div>
@@ -36,12 +37,12 @@ class AddArticleForm extends Component {
 
   returnFoodTemplate() {
     return (
-      <div>
-        <div>
+      <div className="wrap-input-container">
+        <div className="wrap-input-container">
           <label>Food Name: </label>
-          <input type="text" id="foodName" name="foodName" placeholder="np. Pasta" /> 
+          <input type="text" id="foodName" name="foodName" placeholder="eg. Pasta" /> 
         </div>
-        <div>
+        <div className="wrap-input-container">
           <label>Weight: </label>
           <input type="number" id="foodWeight" name="foodWeight" placeholder="Weight" />
         </div>
@@ -51,12 +52,12 @@ class AddArticleForm extends Component {
 
   returnClothesTemplate() {
     return (
-      <div>
-        <div>
+      <div className="wrap-input-container">
+        <div className="wrap-input-container">
           <label>Clothes: </label>
-          <input type="text" id="clothesName" name="clothesName" placeholder="np. Shoes" /> 
+          <input type="text" id="clothesName" name="clothesName" placeholder="eg. Shoes" /> 
         </div>
-        <div>
+        <div className="wrap-input-container">
           <label>Quantity: </label>
           <input type="number" id="quantity" name="quantity" placeholder="Quantity" />
         </div>
@@ -64,28 +65,44 @@ class AddArticleForm extends Component {
     )
   }
 
+  addArticle() {
+    let article = {
+      articleType: this.state.articleType,
+      moneyValue: document.getElementById('moneyValue') ? document.getElementById('moneyValue').value : '',
+      foodName: document.getElementById('foodName') ? document.getElementById('foodName').value : '',
+      foodWeight: document.getElementById('foodWeight') ? document.getElementById('foodWeight').value : '',
+      clothesName: document.getElementById('clothesName') ? document.getElementById('clothesName').value : '',
+      quantity: document.getElementById('quantity') ? document.getElementById('quantity').value : ''
+    };
+    let articles = window.localStorage.getItem('articles') ? [].concat(JSON.parse(window.localStorage.getItem('articles'))) : [];
+    articles.push(article);
+    window.localStorage.setItem('articles', JSON.stringify(articles));
+    window.location = "/company";
+  }
+
   render() {
     return (
       <div className="AddArticleForm">
+        <h2 className="title">Add Article View</h2>
         <Box>
           <form id="addArticleForm" name="addArticleForm">
-            <div className="">
-              <label>Article type</label>
-              <select id="articleType" name="articleType"
-                ref={(select) => { this.articleTypeSelect = select; }}
-                onChange={this.changeArticleType}>
+            <div className="form-container">
+              <div className="wrap-input-container">
+                <label>Article type</label>
+                <select id="articleType" name="articleType"
+                  ref={(select) => { this.articleTypeSelect = select; }}
+                  onChange={this.changeArticleType}>
 
-                <option value="money">Money</option>
-                <option value="food">Food</option>
-                <option value="clothes">Clothes</option>
-              </select>
-              <div>
-                { this.returnTypeArticle() === 'money' ?  this.returnMoneyTemplate() : '' }
-                { this.returnTypeArticle() === 'food' ? this.returnFoodTemplate() : '' }
-                { this.returnTypeArticle() === 'clothes' ? this.returnClothesTemplate() : '' }
+                  <option value="money">Money</option>
+                  <option value="food">Food</option>
+                  <option value="clothes">Clothes</option>
+                </select>
               </div>
-              <div>
-                <button type="button" onClick={this.props.handleAddArticleClick}>Save</button>
+              { this.returnTypeArticle() === 'money' ?  this.returnMoneyTemplate() : '' }
+              { this.returnTypeArticle() === 'food' ? this.returnFoodTemplate() : '' }
+              { this.returnTypeArticle() === 'clothes' ? this.returnClothesTemplate() : '' }
+              <div className="wrap-btn">
+                <button type="button" className="btn" onClick={this.addArticle}>Save</button>
               </div>
             </div>
           </form>
--- src/views/AddArticle.js
+++ src/views/AddArticle.js
@@ -4,23 +4,10 @@ import AddArticleForm from '../components/AddArticleForm';
 
 class AddArticle extends Component {
 
-  // constructor(props) {
-  //   super(props);
-  //   this.handleAddArticleClick = this.handleAddArticleClick.bind(this);
-  //   this.getTypeArticle = this.getTypeArticle.bind(this);
-  // }
-
-  handleAddArticleClick() {
-
-  }
-
   render() {
     return (
       <div className="add-article-view">
-        <h2>Add Article View</h2>
-        <AddArticleForm 
-          onSubmit={this.handleAddArticleClick}>
-        </AddArticleForm>
+        <AddArticleForm></AddArticleForm>
       </div>
     );
   }
