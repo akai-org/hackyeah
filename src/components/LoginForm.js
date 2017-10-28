1e3fabb3d175dbefe5a0dab87245e8649f4caaed
Mateusz Stempniewicz <mateusz.stempniewicz@gmail.com>
2017-10-28 16:58:12 +0200

    add login form component

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -5,18 +5,32 @@ import Box from '../components/Box';
 import './LoginForm.css';
 
 const LoginForm = (props) => (
-  <Box>
-    <form role="form" id="loginForm" novalidate="">
-      <label for="login">Login</label>
-      <input type="text" placeholder="Login" required />
+  <div className="LoginForm">
+    <Box>
+      <div className="login-container">
+        <form role="form" id="loginForm" className="login-form" novalidate="">
+          <div className="wrap-input">
+            <label for="login">Login</label>
+            <input type="text" placeholder="Login" required />
+          </div>  
 
-      <label for="password">Password</label>
-      <input type="password" placeholder="Password" required />
+          <div className="wrap-input">
+            <label for="password">Password</label>
+            <input type="password" placeholder="Password" required />
+          </div>
+          
+          <div className="wrap-input wrap-checkbox">
+            <input type="checkbox" />
+            <label>Remember Me</label>
+          </div>
 
-      <input type="checkbox" /><label>Remember Me</label>
-      <button type="submit">Login</button>
-    </form>
-  </Box>
+          <div className="wrap-btn">
+            <button type="submit" class="btn btn-login">Login</button>
+          </div>
+        </form>
+      </div>
+    </Box>
+  </div>
 );
 
 export default LoginForm;
