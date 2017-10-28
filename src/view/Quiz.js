74c9e4f4455aa50a08bc5d8fe2566302dfb074a2
Rafał Rudol <rrrrudol@gmail.com>
2017-10-28 23:26:15 +0200

    add method getTask

--- src/view/Quiz.js
+++ src/view/Quiz.js
@@ -3,15 +3,38 @@ import React, { Component } from 'react';
 class Quiz extends Component {
   constructor() {
     super();
+    // this.state = {
+    //   question: '',
+    //   answers: [ '', '', '', '' ],
+    //   image: ''
+    // }
+  }
 
-    this.state = {
-      question: '',
-      answers: [ '', '', '', '' ],
-      image: ''
-    }
+  getTask(id) {
+    fetch(`https://hackyeah-b8a56.firebaseio.com/x-nature-quiz/questions/${id}.json`)
+      .then( (response) => {
+        return response.json();
+      })
+      .then( (response) => {
+        let newState = { question: response.question, image: response.image, answers: [] };
+        for(const i in response.answers){
+          console.log(i);
+          newState.answers.push(response.answers[i]);
+        }
+
+        console.log(newState);
+        this.setState(newState);
+      })
+      .catch( (err) => {
+        alert('Nastąpił błąd podczas pobierania danych.');
+      });
   }
 
   render() {
+    if(!this.state) {
+      this.getTask(0);
+      return <div>Loading</div>;
+    }
     return (
       <div className="Quiz">
         <div className="question">
