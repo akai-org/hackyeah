1266babad8fd89dfef180ab91cb5d9dea1e408e4
poe <robakzdobywca@gmail.com>
2017-10-28 22:41:41 +0200

    donate view + styling update

--- src/views/Donate.js
+++ src/views/Donate.js
@@ -1,24 +1,41 @@
 import React from 'react';
-
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
 import Footer from '../components/Footer';
 import Box from '../components/Box';
+import './Home.css';
 
 const Donate = () => (
   <div className="donate-view">
+
     <Header/>
-    <Box variant="small color-a" >
+
+    <Box variant="large color-d">
       <header className="Box-header">
-        <h2>Share your foods</h2>
+        <h2>Donut your money!</h2>
       </header>
       <main className="Box-content">
-        <img src="/images/money.png" alt="" />
-        <p>With us you can share foods with people in need.</p>
+        <img src="/images/donut.png" alt="" />
+        <p>Thank you!</p>
+        <footer className="Box-footer">
+          <div className="donate-buttons">
+            <div className="Box-button">
+              <a href="#">Donate 1 PLN</a>
+            </div>
+            <div className="Box-button">
+              <a href="#">Donate 5 PLN</a>
+            </div>
+            <div className="Box-button">
+              <a href="#">Donate 50 PLN</a>
+            </div>
+            <div className="Box-button">
+              <a href="#">Donate 100 PLN</a>
+            </div>
+          </div>
+        </footer>
       </main>
-      <footer className="Box-footer">
-        <img src="http://f4ad.com/wp-content/uploads/2013/03/paypal-donate-button11.png" alt="Donate"/>
-      </footer>
     </Box>
+
     <Footer/>
   </div>
 );
