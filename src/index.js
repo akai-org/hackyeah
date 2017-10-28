9a04b88d0bba141f4a229da4368c2590bda322ab
Rafał Rudol <rrrrudol@gmail.com>
2017-10-28 14:47:25 +0200

    add NotFound component

--- src/index.js
+++ src/index.js
@@ -1,21 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Route, Router } from 'react-router-dom'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 import Header from './components/Header';
 import Home from './views/Home';
+import NotFound from './views/NotFound';
 
 ReactDOM.render(
   <BrowserRouter>
-    <div>
-      <Route path='/' component={Header}/>
+    <Switch>
       <Route exact path='/' component={Home}/>
       <Route path='/magic' component={Home}/>
-      <Route path='/magic' component={Home}/>
-    </div>
+      <Route component={NotFound} />
+    </Switch>
   </BrowserRouter>,
   document.getElementById('root'));
 registerServiceWorker();
---
+++ src/views/NotFound.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotFound = (props) => (
+  <header className="NotFound">
+    <h2>Ups 404</h2>
+  </header>
+);
+
+export default NotFound;
