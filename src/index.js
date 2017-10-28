bca3f81c907b3638d47d8715e44607cbab29abe7
Rafał Rudol <rrrrudol@gmail.com>
2017-10-28 14:39:41 +0200

    add router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Router } from 'react-router-dom'
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import Header from './components/Header';
+import Home from './views/Home';
+
+ReactDOM.render(
+  <BrowserRouter>
+    <div>
+      <Route path='/' component={Header}/>
+      <Route exact path='/' component={Home}/>
+      <Route path='/magic' component={Home}/>
+      <Route path='/magic' component={Home}/>
+    </div>
+  </BrowserRouter>,
+  document.getElementById('root'));
 registerServiceWorker();
