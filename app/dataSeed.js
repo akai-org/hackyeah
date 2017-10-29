ea4e539112f5f68ca1b40ff8229aa4577a23f1cc
Rafał Rudol <rrrrudol@gmail.com>
2017-10-29 02:37:41 +0100

    upgrade

--- app/dataSeed.js
+++ app/dataSeed.js
@@ -1,3 +1,6 @@
 module.exports = (db) => {
   db.push('/app/title', 'Happy Helping');
+  db.push('/app/stats/first', 20000);
+  db.push('/app/stats/second', 2500);
+  db.push('/app/stats/third', 6000);
 };
